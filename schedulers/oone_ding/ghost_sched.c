6dc085b62cc1268e6cb6a8e42055dace1e66c75f
Dingwoonee <slm0523@naver.com>
2024-10-09 23:50:16 +0900

    fix ghost_sched.g

--- schedulers/oone_ding/ghost_sched.c
+++ schedulers/oone_ding/ghost_sched.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 #include <sched.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <dlfcn.h>
 
@@ -7,8 +8,9 @@
 #define SCHED_GHOST 18
 #endif
 
+typedef int (*orig_pthread_create_type)(pthread_t *, const pthread_attr_t *, void *(*)(void *), void *);
+
 int sched_setscheduler(pid_t pid, int policy, const struct sched_param *param) {
-    // 원래의 sched_setscheduler 함수 호출을 가로채서 SCHED_GHOST로 강제 설정
     static int (*real_sched_setscheduler)(pid_t, int, const struct sched_param *);
     if (!real_sched_setscheduler) {
         real_sched_setscheduler = dlsym(RTLD_NEXT, "sched_setscheduler");
@@ -16,4 +18,19 @@ int sched_setscheduler(pid_t pid, int policy, const struct sched_param *param) {
     struct sched_param ghost_param = { .sched_priority = 0 };
     printf("Forcing process to use SCHED_GHOST...\n");
     return real_sched_setscheduler(pid, SCHED_GHOST, &ghost_param);
+}
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg) {
+    static orig_pthread_create_type real_pthread_create = NULL;
+    if (!real_pthread_create) {
+        real_pthread_create = (orig_pthread_create_type)dlsym(RTLD_NEXT, "pthread_create");
+    }
+    printf("Creating thread with SCHED_GHOST policy...\n");
+    int ret = real_pthread_create(thread, attr, start_routine, arg);
+
+    // After thread creation, set it to SCHED_GHOST
+    struct sched_param param = { .sched_priority = 0 };
+    sched_setscheduler(0, SCHED_GHOST, &param);
+
+    return ret;
 }
